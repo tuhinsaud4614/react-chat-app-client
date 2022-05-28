ddf9dfd3e857d3d2bbb4e6a4319fc08f17f1f5b2
tuhinsaud4614@gmail.com <tuhinsaud4614@gmail.com>
2022-05-28 23:26:36 +0600

    Component:Modal
    Modal Component update

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 import * as React from "react";
 import { useSplitElement } from "../../hooks";
 import Portal from "../Portal";
@@ -8,10 +9,40 @@ import Foot from "./Foot";
 import Head from "./Head";
 
 const className = {
-  backdrop: "fixed z-[900] inset-0 bg-black/50",
+  backdrop: "fixed z-[900] inset-0 bg-gray-500 bg-opacity-75",
   center: "flex items-center justify-center",
   container:
-    "flex flex-col bg-white shadow-mine-2 rounded-2xl max-h-[calc(100%-16px)] w-full sm:max-w-[calc(640px-32px)] m-4 sm:mx-auto overflow-x-hidden",
+    "flex flex-col bg-white shadow-mine-2 rounded-2xl max-h-[calc(100%-32px)] w-full sm:max-w-[calc(640px-32px)] m-4 sm:mx-auto overflow-x-hidden",
+};
+
+const backdropVariants: Variants = {
+  start: {
+    opacity: 0,
+  },
+  end: {
+    opacity: 1,
+  },
+};
+
+const containerVariants: Variants = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
 };
 
 interface Props {
@@ -42,28 +73,39 @@ function ModalComponent({
   });
   return (
     <Portal>
-      {open && (
-        <div
-          onClick={!staticBack ? onHide : undefined}
-          className={classNames(
-            className.backdrop,
-            center && className.center,
-            !staticBack && "cursor-pointer",
-            classes?.backdrop
-          )}
-        >
-          <ModalContext.Provider value={{ onHide: onHide }}>
-            <div
-              onClick={(e) => e.stopPropagation()}
-              className={classNames(className.container, classes?.container)}
-            >
-              {head}
-              {body}
-              {foot}
-            </div>
-          </ModalContext.Provider>
-        </div>
-      )}
+      <AnimatePresence exitBeforeEnter>
+        {open && (
+          <motion.div
+            role="dialog"
+            variants={backdropVariants}
+            initial="start"
+            animate="end"
+            exit="start"
+            onClick={!staticBack ? onHide : undefined}
+            className={classNames(
+              className.backdrop,
+              center && className.center,
+              !staticBack && "cursor-pointer",
+              classes?.backdrop
+            )}
+          >
+            <ModalContext.Provider value={{ onHide: onHide }}>
+              <motion.div
+                variants={containerVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                onClick={(e) => e.stopPropagation()}
+                className={classNames(className.container, classes?.container)}
+              >
+                {head}
+                {body}
+                {foot}
+              </motion.div>
+            </ModalContext.Provider>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </Portal>
   );
 }
