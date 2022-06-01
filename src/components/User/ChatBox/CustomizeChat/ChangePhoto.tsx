d2536be1039a7acbe07a0eee866a70486686a8d5
tuhinsaud4614@gmail.com <tuhinsaud4614@gmail.com>
2022-06-01 21:34:35 +0600

    Component:ChatBox
    ChangePhoto created

---
+++ src/components/User/ChatBox/CustomizeChat/ChangePhoto.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { BsCardImage } from "react-icons/bs";
+import OptionsItem from "../OptionsItem";
+
+export default function ChangePhoto() {
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
+
+  const clickHandler = () => {
+    inputRef.current?.click();
+  };
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.files && event.currentTarget.files.length === 1) {
+      const file = event.currentTarget.files[0];
+      const type = file.type.replace("image/", "");
+      if (
+        !["jpg", "png", "jpeg", "gif"].includes(type) ||
+        file.size > 5 * 1000000
+      ) {
+        console.log("invalid image");
+        return;
+      }
+    }
+  };
+
+  return (
+    <OptionsItem
+      icon={BsCardImage}
+      ariaLabel="Change photo"
+      onClick={clickHandler}
+    >
+      Change photo
+      <input
+        ref={inputRef}
+        type="file"
+        accept=".jpg,.png,.jpeg,.gif"
+        className="hidden"
+        onChange={changeHandler}
+      />
+    </OptionsItem>
+  );
+}
--- src/components/User/ChatBox/CustomizeChat/index.tsx
+++ src/components/User/ChatBox/CustomizeChat/index.tsx
@@ -1,20 +1,18 @@
 import * as React from "react";
 import { BiLike } from "react-icons/bi";
-import { BsCardImage } from "react-icons/bs";
 import OptionsItem from "../OptionsItem";
 import OptionsItems from "../OptionsItems";
 import ChangeChatName from "./ChangeChatName";
+import ChangePhoto from "./ChangePhoto";
 import EditNickNames from "./EditNicknames";
 
-interface CustomizeChatProps {}
-export default function CustomizeChat({}: CustomizeChatProps) {
+// interface CustomizeChatProps {}
+// export default function CustomizeChat({}: CustomizeChatProps) {
+export default function CustomizeChat() {
   return (
     <OptionsItems title="Customize chat">
       <ChangeChatName />
-      <OptionsItem icon={BsCardImage} ariaLabel="Change photo">
-        Change photo
-        <input accept=".jpg,.png,.jpeg" className="hidden" />
-      </OptionsItem>
+      <ChangePhoto />
       <OptionsItem icon={BiLike}>Change emoji</OptionsItem>
       <EditNickNames />
     </OptionsItems>
